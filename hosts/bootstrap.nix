452711b8078449799a0bd8e9e55cd7c29f7769df
Caleb Norton <n0603919@outlook.com>
2025-01-28 21:06:45 -0600

    Bootstrap first machine

--- hosts/bootstrap.nix
+++ hosts/bootstrap.nix
@@ -1,8 +1,10 @@
 {
   imports = [
     ./global.nix
+    ../hardware/wyse-disko.nix
   ];
 
-  # generate by running nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`
+  # generate by running nixos-anywhere with `--generate-hardware-config nixos-facter output.json`
   facter.reportPath = ../hardware/wyse-5070.json;
+  users.users.root.initialPassword = "bootstrap";
 }
