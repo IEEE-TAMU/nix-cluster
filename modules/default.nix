a99c2da19c6d4d89ef23b8b6717ce0ae47d63318
Caleb Norton <n0603919@outlook.com>
2025-01-29 18:48:24 -0600

    rename cluster-node module

--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,5 @@
 {
-  cluster = import ./cluster.nix;
+  cluster-node = import ./cluster-node.nix;
   haproxy = import ./haproxy.nix;
   k3s = import ./k3s.nix;
   keepalived = import ./keepalived.nix;
