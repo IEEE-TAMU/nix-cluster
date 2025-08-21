0a3488320c79c4bd0f4e6d60128e5a6e55409609
Caleb Norton <n0603919@outlook.com>
2025-08-21 13:53:38 -0500

    add IEEE PC ssh key

--- hosts/global.nix
+++ hosts/global.nix
@@ -14,9 +14,15 @@
   sops.secrets.root_password.neededForUsers = true;
   users.mutableUsers = false;
 
-  users.users.root.hashedPasswordFile = config.sops.secrets.root_password.path;
+  users.users.root = {
+    hashedPasswordFile = config.sops.secrets.root_password.path;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2FLDIautZl87H9xJKsPJsO0gO/8t4jOS3Szz4j2qY4"
+    ];
+  };
+
   services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "yes";
+
   environment.systemPackages = with pkgs; [
     git
     vim
