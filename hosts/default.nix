a6dc6a053bdaaa900b469b04b3778d730c5655ab
Caleb Norton <n0603919@outlook.com>
2025-12-16 13:55:13 -0600

    add laptop to authorized keys

--- hosts/default.nix
+++ hosts/default.nix
@@ -30,7 +30,8 @@
       users.users.root = {
         hashedPasswordFile = config.sops.secrets.root_password.path;
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2FLDIautZl87H9xJKsPJsO0gO/8t4jOS3Szz4j2qY4"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2FLDIautZl87H9xJKsPJsO0gO/8t4jOS3Szz4j2qY4 IEEE@IEEEPC"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3tGxUsgEJN/dwJ+QovVJd0yNg+YkJercIjGVJD+rvt caleb@chnorton-fw"
         ];
       };
 
